21cf8768c5eb00df51926dbfc4d6c3623d44dcdb
ddeepesh91 <73163195+ddeepesh91@users.noreply.github.com>
2021-08-24 14:12:55 +0200

    Add few more extensions

--- custom.js
+++ custom.js
@@ -1,15 +1,28 @@
-
+window.OPENSHIFT_CONSTANTS.DISABLE_COPY_LOGIN_COMMAND = true;
 window.OPENSHIFT_CONSTANTS.GUIDED_TOURS.landing_page_tour.auto_launch = false;
 window.OPENSHIFT_CONSTANTS.HELP['get_started_cli']      = "https://www.vodafone.de/";
 window.OPENSHIFT_CONSTANTS.HELP['basic_cli_operations'] = "https://www.vodafone.de/";
 window.OPENSHIFT_CONSTANTS.HELP_BASE_URL = "https://www.vodafone.de/"; 
 window.OPENSHIFT_CONSTANTS.CATALOG_HELP_RESOURCES.links.push({
   title: 'Blog',
-  href: 'https://blog.openshift.com'
+  href: 'https://www.vodafone.de/'
 });
 
 
-var applicationsMenu = _.find(window.OPENSHIFT_CONSTANTS.PROJECT_NAVIGATION, { label: 'Applications' });
-applicationsMenu.secondaryNavSections.push({ // Add a new secondary nav section to the Applications menu
-  label: 'Testing'
-});
+window.OPENSHIFT_CONSTANTS.CLI = {
+  "Latest Release": "https://www.vodafone.de/"
+};
+
+angular
+  .module('aboutPageExtension', ['openshiftConsole'])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/about', {
+        templateUrl: 'https://example.com/extensions/about/about.html',
+        controller: 'AboutController'
+      });
+    }
+  );
+
+hawtioPluginLoader.addModule('aboutPageExtension');
+
